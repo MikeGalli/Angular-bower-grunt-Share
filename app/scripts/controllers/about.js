7b05464952cf7e93b05fb2c3a688ff21099821f8
Mike G <mkgalligar@gmail.com>
2017-02-28 14:13:02 -0800

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularBowerGruntShareApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularBowerGruntShareApp
+ */
+angular.module('angularBowerGruntShareApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularBowerGruntShareApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularBowerGruntShareApp
+ */
+angular.module('angularBowerGruntShareApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
